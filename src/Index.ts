8e7e0f2ec18f13b38c8f7a41a5f3159a000280c4
JosueSalomon <josueisacsalomonlanda@gmail.com>
2025-05-04 22:16:51 -0600

    first commit

---
+++ src/Index.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+require('dotenv').config();
+const app: Express = express();
+const port = process.env.PORT;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Servidor raiz is on');
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto: ${port}`);
+});
